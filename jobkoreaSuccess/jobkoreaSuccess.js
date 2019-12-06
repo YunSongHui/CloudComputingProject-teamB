1af743dffdd64a515050e6bca86ed4f707da8ae3
nohgijin <ynda11111@naver.com>
2019-12-07 03:04:17 +0900

    잡코리아 합격스펙 가져오기

--- jobkoreaSuccess/jobkoreaSuccess.js
+++ jobkoreaSuccess/jobkoreaSuccess.js
@@ -1,26 +1,33 @@
 const puppeteer = require('puppeteer');
-var page
 
-async function getData() {
-    var data = await page.evaluate(() => {
+let curPage = 1
+
+var page
+var subpage
+
+async function getSpecData(link) {
+    await subpage.goto(link)
+    var data = await subpage.evaluate(() => {
         function getData() {
 
-            var list = document.querySelectorAll('tab03 > article.artReadStrategy > div > div > div.devStartlist.listArea.specList > div > div.specListWrap > div > ul')
+            var list = document.querySelectorAll('#container > div.stContainer > div.specViewWrap > div.specAtc.myTotalAtc > div.averageSpecWrap > div.averageSpec.allContent > div.specListWrap > div > ul')
             var result = []
 
-            list.forEach(div => {
-                var grade = document.querySelector('li:nth-child(1) > div > span').innerText
-                var toeic = document.querySelector('li:nth-child(2) > div > span').innerText
-                var toeicSpeaking = document.querySelector('li:nth-child(3) > div > span').innerText
-                var opic = document.querySelector('li:nth-child(4) > div > span').innerText
-                var language = document.querySelector('li:nth-child(5) > div > span').innerText
-                var certificate = document.querySelector('li:nth-child(6) > div > span').innerText
-                var oversea = document.querySelector('li:nth-child(7) > div > span').innerText
-                var intern = document.querySelector('li:nth-child(8) > div > span').innerText
-                var award = document.querySelector('li:nth-child(9) > div > span').innerText
-                var volunteer = document.querySelector('li:nth-child(10) > div > span').innerText
-    
+            list.forEach(ul => {
+                var company=document.querySelector('#devPassSpecForm > div > h2 > strong > a').innerText
+                var grade = ul.querySelector('li:nth-child(1) > div > span').innerText
+                var toeic = ul.querySelector('li:nth-child(2) > div > span').innerText
+                var toeicSpeaking = ul.querySelector('li:nth-child(3) > div > span').innerText
+                var opic = ul.querySelector('li:nth-child(4) > div > span').innerText
+                var language = ul.querySelector('li:nth-child(5) > div > span').innerText
+                var certificate = ul.querySelector('li:nth-child(6) > div > span').innerText
+                var oversea = ul.querySelector('li:nth-child(7) > div > span').innerText
+                var intern = ul.querySelector('li:nth-child(8) > div > span').innerText
+                var award = ul.querySelector('li:nth-child(9) > div > span').innerText
+                var volunteer = ul.querySelector('li:nth-child(10) > div > span').innerText
+
                 var data = {
+                    company,
                     grade,
                     toeic,
                     toeicSpeaking,
@@ -42,39 +49,42 @@ async function getData() {
 }
 
 async function nextPage() {
-    if (!await page.$('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(1) > a')) return false;
-    await page.evaluate(() => {
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(1) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(2) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(3) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(4) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(5) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(6) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(7) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(8) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(9) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > ul > li:nth-child(10) > a').click()
-        document.querySelector('#container > div.stContainer > div.starListsWrap.ctTarget > div.tplPagination > p > a').click()
-    })
+    await page.goto(`http://www.jobkorea.co.kr/starter/spec?IsFavorOn=0&IsAlumniOn=0&Page=${curPage}`)
     await page.waitFor(3000)
-    return true
+    curPage++
+
+    var link_list = await page.evaluate(() => {
+        function getData() {
+
+            var list = document.querySelectorAll('#container > div.stContainer > div.starListsWrap.ctTarget > div.specListArea > ul > li > div.coWrap > dl > dt > a.tit')
+            var result = []
+
+            list.forEach(a => {
+                var link = a.href
+                result.push(link)
+            });
+            return result
+        }
+        return getData()
+    })
+
+    console.log(link_list)
+
+    for (const link of link_list) {
+        let spec = await getSpecData(link)
+        console.log(spec)
+    }
+
+    await nextPage()
 }
 
 (async () => {
     const browser = await puppeteer.launch({
-        //        headless: false
+        headless: false
     });
     page = await browser.newPage();
+    subpage = await browser.newPage();
     await page.goto('http://www.jobkorea.co.kr/starter/spec');
-
-    var data_list = []
-
-    while (true) {
-        var data = await getData()
-        data_list.push(data)
-        console.log(data)
-        
-        if (!await nextPage()) break;
-    }
-    await browser.close();
-})();
+    await nextPage()
+    // setInterval(nextPage, 2000)
+})()
